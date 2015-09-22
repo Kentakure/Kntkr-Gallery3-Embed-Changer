680007b20f878e26c138f3003e71644a2c1881d4
Kentakure <Kentakure@users.noreply.github.com>
2015-09-22 15:52:45 +0900

    Update kntkrg3ec.php

--- kntkrg3ec.php
+++ kntkrg3ec.php
@@ -11,5 +11,16 @@ License: KENTAKURE
 /*編集ここから*/
 include("kntkrg3ec-option.php");
 require_once("kntkrg3ec-code.php");
+//--------------------------------------------------------------------------
+//
+//  プラグイン削除の際に行うオプションの削除
+//
+//--------------------------------------------------------------------------
+if ( function_exists('register_uninstall_hook') ) {
+    register_uninstall_hook(__FILE__, 'uninstall_hook_kntkrg3ec');
+}
+function uninstall_hook_kntkrg3ec () {
+    delete_option('kntkrg3ec_siteindex');
+}
 /*編集ここまで*/
 ?>
