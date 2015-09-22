26bff3dfc6b578691d9d8566c0b95068baa9fa35
Kentakure <Kentakure@users.noreply.github.com>
2015-09-22 15:04:43 +0900

    Update and rename .gitignore to kntkrg3ec.php

---
+++ kntkrg3ec.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Kntkr Gallery3 Embed Changer
+Plugin URI: http://kentakure.net
+Description: 指定したGallery3のURLを自動的に画像に差し替える。設定画面付き。
+Version: 1.0
+Author: KENTAKURE
+Author URI: http://kentakure.net
+License: KENTAKURE
+*/
+/*編集ここから*/
+include("kntkrg3ec-option.php");
+require_once("kntkrg3ec-code.php");
+/*編集ここまで*/
+?>
