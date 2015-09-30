a0ac36f34614ff149d2f1b18faf7d588665596b4
Kentakure <Kentakure@users.noreply.github.com>
2015-10-01 04:42:03 +0900

    Create kntkrg3ec-code.php

---
+++ kntkrg3ec-code.php
@@ -0,0 +1,32 @@
+<?php
+function kntkr_gallery3_embed_changer($the_content){
+//Gallery3を設置したディレクトリのURL
+$option_siteindex = get_option('kntkrg3ec_siteindex');
+$resizes = 'http://'.$option_siteindex.'/var/resizes/';
+$thumbs = 'http://'.$option_siteindex.'/var/thumbs/';
+//URLの正規表現文字列化
+$sitepattern = '(https?|ftp)\:\/\/'.preg_quote($option_siteindex,'/');
+//本文中から指定したドメインが検出された場合のみ下記のスクリプトが発動。
+if(preg_match('{'.$sitepattern.'}',$the_content)){
+	//特定のAタグの解除
+	$convert1 = preg_replace('{<a(.*)?href\=\"'.$sitepattern.'\/(?!photos|var)([-_.!~*\'()a-zA-Z0-9;\/?:@&=+$,%#]+)\"([^<>]*)?>(.*)?<\/a>}','$5',$the_content);
+	//IMGタグの整形。大きさをリサイズに統一。Class属性等が記述されている場合は継承。
+	$convert2 = preg_replace('{(<a.*>)?<img(.*)?src\=\"'.$sitepattern.'\/var\/(albums|resizes|thumbs)\/([-_.!~*\'()a-zA-Z0-9;\/:@&=+$,%#]+)\.([^?"<>]*)(?:\?[^"<>]*)?\"([^<>]*)?>(<\/a>)?}', '<div class="kntkrg3ec" style="text-align:center;">$1<img$2src="$3://'.$option_siteindex.'/var/resizes/$5.$6"$7>$8</div>', $convert1);
+	//拡張子が付いていないURL（アドレスバーからのコピペ）の変換。
+	$convert3 = preg_replace('{'.$sitepattern.'\/(?!photos|var\/albums|var\/resizes|var\/thumbs)([-_.!~*\'()a-zA-Z0-9;\/?:@&=+$,%#]+)}','<div class="kntkrg3ec" style="text-align:center;"><a href="'.$resizes.'$2" rel="nofollow"><img src="'.$resizes.'$2"></a></div>',$convert2);
+	//モバイル判別
+	if(wp_is_mobile()){
+	//スマホでのアクセスの場合には、サムネイル画像を代用。
+	$convert4 = preg_replace('{<img(.*)?src\=\"'.$sitepattern.'\/var\/resizes\/([-_.!~*\'()a-zA-Z0-9;\/:@&=+$,%#]+)(?:\?[^"<>]*)?\"([^<>]*)?>}','<img$1src="'.$thumbs.'$3"$4>',$convert3);
+	//代用したサムネイル画像のリンク先を新規ウィンドウで開くに設定変更。
+	$convert5 = preg_replace('{<a(.*)?href\=\"'.$sitepattern.'\/var\/resizes\/([-_.!~*\'()a-zA-Z0-9;\/:@&=+$,%#]+)\"([^<>]*)?><img(.*)?><\/a>}','<a$1href="'.$resizes.'$3"$4target="_blank"><img$5></a>',$convert4);
+	$the_content = $convert5;
+	}else{
+	//PC表示の場合にはリサイズのままで申し送る。
+	$the_content = $convert3;
+	}
+}
+return $the_content;
+}
+add_filter('the_content','kntkr_gallery3_embed_changer', 1);
+?>
